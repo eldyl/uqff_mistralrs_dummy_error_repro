d8cd7338cde5513086038ff3e8a1234c17ff79d0
Dylan Ullrich <dylan@eldyl.org>
2025-05-19 15:06:11 -0700

    add phi and mistral variations

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 use anyhow::Result;
 use mistralrs::{
     IsqType, PagedAttentionMetaBuilder, RequestBuilder, TextMessageRole, TextMessages,
@@ -6,12 +8,69 @@ use mistralrs::{
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    phi().await
+    // mistral().await
+}
+
+async fn phi() -> Result<()> {
     let model = UqffTextModelBuilder::new(
         "EricB/Phi-3.5-mini-instruct-UQFF",
         vec!["phi3.5-mini-instruct-q8_0.uqff".into()],
     )
     .into_inner()
     .with_isq(IsqType::Q8_0)
+    .with_logging()
+    .with_paged_attn(|| PagedAttentionMetaBuilder::default().build())?
+    .build()
+    .await?;
+
+    let messages = TextMessages::new()
+        .add_message(
+            TextMessageRole::System,
+            "You are an AI agent with a specialty in programming.",
+        )
+        .add_message(
+            TextMessageRole::User,
+            "Hello! How are you? Please write generic binary search function in Rust.",
+        );
+
+    let response = model.send_chat_request(messages).await?;
+
+    println!("{}", response.choices[0].message.content.as_ref().unwrap());
+    dbg!(
+        response.usage.avg_prompt_tok_per_sec,
+        response.usage.avg_compl_tok_per_sec
+    );
+
+    // Next example: Return some logprobs with the `RequestBuilder`, which enables higher configurability.
+    let request = RequestBuilder::new().return_logprobs(true).add_message(
+        TextMessageRole::User,
+        "Please write a mathematical equation where a few numbers are added.",
+    );
+
+    let response = model.send_chat_request(request).await?;
+
+    println!(
+        "Logprobs: {:?}",
+        &response.choices[0]
+            .logprobs
+            .as_ref()
+            .unwrap()
+            .content
+            .as_ref()
+            .unwrap()[0..3]
+    );
+
+    Ok(())
+}
+
+async fn mistral() -> Result<()> {
+    let model = UqffTextModelBuilder::new(
+        "EricB/Mistral-7B-Instruct-v0.3-UQFF",
+        vec!["mistral0.3-7b-instruct-q4k.uqff".into()],
+    )
+    .into_inner()
+    .with_isq(IsqType::Q4K)
     .with_logging()
     .with_paged_attn(|| PagedAttentionMetaBuilder::default().build())?
     .build()
